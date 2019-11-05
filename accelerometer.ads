35e1467d4b26ef8cbb9567436b5f136147008e33
212135 <54791363+212135@users.noreply.github.com>
2019-11-05 14:18:06 +0100

    changes pins in sensor adb

--- accelerometer.ads
+++ accelerometer.ads
@@ -1,21 +1,23 @@
-package Accelerometer is
-     
-   
+with Microbit.Time;    use type MicroBit.Time.Time_Ms;
+
+
+package accelerometer is
 
     type acc_data is range -2 ** 9 .. 2 ** 9 - 1;
     
-   
-   -- used to stop the car when it is picked up
-   tilt_max_y: constant acc_data := -100;  -- old working value -220 or 200
-   tilt_min_y: constant acc_data := -300;  -- old working value -260 or 300
-   
-   
-    procedure use_display;
+    -- used to stop the car when it is picked up
+    tilt_max_y: constant acc_data := -200;  -- old working value -220 or 200
+    tilt_min_y: constant acc_data := -300;  -- old working value -260 or 300
+    
+    procedure Initialize;
+    function Initialized return Boolean;
+
+    procedure show_on_display;
 
     function get_x return acc_data;
     function get_y return acc_data;
     function get_z return acc_data;
 
-end Accelerometer;
+    function check_acc (Deadline : MicroBit.Time.Time_Ms) return Integer;
 
---pragma
+end accelerometer;
--- sensor.adb
+++ sensor.adb
@@ -1,55 +1,104 @@
 with MicroBit.IOs; use MicroBit.IOs;
-with MicroBit.Time;
+with MicroBit.Servos;
+
 
 
 package body Sensor is
 
-    --  PINS
+   --  PINS
 
-   -- just playing here  copy Suri
-  
-    sensor_TRIG_pin: constant Pin_Id := 4;  -- sending the sensor
-    sensor_ECHO_pin: constant Pin_Id := 3;  -- receiving from the sensor
+   sensor_TRIG_pin : constant Pin_Id := 4;  -- sending the sensor
+   sensor_ECHO_pin : constant Pin_Id := 3;  -- receiving from the sensor
+   servo_pin : constant Pin_Id := 1; -- PWM signal to servo via this pin
+                                          
+   -- VARS
+   duration_echo_us : travel_time_us; -- Duration of the pulse from ECHO pin measured in MicroSeconds
+   servo_delay : constant MicroBit.Time.Time_Ms := 500;
 
-    -- VARS
-    duration_echo_us: travel_time_us; -- Duration of the pulse from ECHO pin measured in MicroSeconds
+
+   function read return travel_time_us is
+   begin
+    
+      duration_echo_us := 0;
+
+      MicroBit.IOs.Set(sensor_TRIG_pin, False);
+      MicroBit.Time.HAL_Delay.Delay_Microseconds(10);
+
+      MicroBit.IOs.Set(sensor_TRIG_pin, True);
+      MicroBit.Time.HAL_Delay.Delay_Microseconds(10);
+      MicroBit.IOs.Set(sensor_TRIG_pin, False);
+
+      while true loop
+
+         if MicroBit.IOs.Set(sensor_ECHO_pin) then
+
+            MicroBit.Time.HAL_Delay.Delay_Microseconds(1);
+            if MicroBit.IOs.Set(sensor_ECHO_pin) then
+               duration_echo_us := duration_echo_us + 1;
+            end if;
+
+         end if;
+
+      end loop;
+      return duration_echo_us;
+	
+   end read;
 
 
-    function read return travel_time_us is
-    begin
-	duration_echo_us := 10;
+   function read (Deadline : MicroBit.Time.Time_Ms) return Integer is
+      start_time : constant MicroBit.Time.Time_Ms := MicroBit.Time.Clock;
+   begin
 
-	MicroBit.IOs.Set(sensor_TRIG_pin, False);
-	MicroBit.Time.Delay_Ms(100);
+      duration_echo_us := 0;
 
-	MicroBit.IOs.Set(sensor_TRIG_pin, True);
-	MicroBit.Time.HAL_Delay.Delay_Microseconds(10);
-        MicroBit.IOs.Set(sensor_TRIG_pin, False);
-      
-     
+      MicroBit.IOs.Set(sensor_TRIG_pin, False);
+      MicroBit.Time.HAL_Delay.Delay_Microseconds(10);
 
-	outerloop:
-      loop  -- if sensor_ECHO_pin is false all the time the loop never ends.
+      MicroBit.IOs.Set(sensor_TRIG_pin, True);
+      MicroBit.Time.HAL_Delay.Delay_Microseconds(10);
+      MicroBit.IOs.Set(sensor_TRIG_pin, False);
 
-	    if MicroBit.IOs.Set(sensor_ECHO_pin) then
+      while MicroBit.Time.Clock - start_time < Deadline loop
 
-		innerloop:
-		loop
-		    MicroBit.Time.HAL_Delay.Delay_Microseconds(1);
-		    if MicroBit.IOs.Set(sensor_ECHO_pin) then
-			duration_echo_us := duration_echo_us + 1;
-		    else
-			return duration_echo_us;
-		    end if;
-            end loop innerloop;
-            
-            elsif not MicroBit.IOs.Set(sensor_ECHO_pin) then
-		   return duration_echo_us;
-         end if;	 
-         -- return duration_echo_us; Maybe it should be here!
-	end loop outerloop;
+         if MicroBit.IOs.Set(sensor_ECHO_pin) then
 
-    end read;
+            MicroBit.Time.HAL_Delay.Delay_Microseconds(1);
+		
+            if MicroBit.IOs.Set(sensor_ECHO_pin) then
+               duration_echo_us := duration_echo_us + 1;
+            end if;
+
+         end if;
+
+      end loop;
+      return Integer(duration_echo_us);
+	
+   end read;
+
+
+   function sensor_straight (Deadline :  MicroBit.Time.Time_Ms) return Integer is
+   begin
+      Microbit.Servos.Go(servo_pin,45);
+--        MicroBit.Time.Delay_Ms(servo_delay);
+--        MicroBit.Servos.Stop(servo_pin);
+      return read(Deadline);
+   end sensor_straight;
    
+   function sensor_left (Deadline :  MicroBit.Time.Time_Ms) return Integer is
+   begin
+      Microbit.Servos.Go(servo_pin,90);
+--        MicroBit.Time.Delay_Ms(servo_delay);
+--        MicroBit.Servos.Stop(servo_pin);
+      return read(Deadline);
+   end sensor_left;
+
+   function sensor_right (Deadline :  MicroBit.Time.Time_Ms) return Integer is
+   begin
+      Microbit.Servos.Go(servo_pin,0);
+--        MicroBit.Time.Delay_Ms(servo_delay);
+--        MicroBit.Servos.Stop(servo_pin);
+      return read(Deadline);
+   end sensor_right;
+    
    
 end Sensor;
